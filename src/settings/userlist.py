e2e0f37478401bfcd142d0ff2a8c2324cb591e92
aliriza <aliriza.keskin@pardus.org.tr>
2023-04-06 13:21:16 +0000

    user list settigs

---
+++ src/settings/userlist.py
@@ -0,0 +1,18 @@
+# Lightdm
+self.settings["userlist.json"].set_data("enabled",{
+    "label":_("Enable username list"),
+     "value": config.get("enabled","true")
+})
+self.settings["userlist.json"].set_data("show-realname",{
+    "label":_("Show real names"),
+     "value": config.get("show-realname","true")
+})
+self.settings["userlist.json"].set_data("user-hide-button",{
+    "label":_("Enable hide user button"),
+     "value": config.get("user-hide-button","true")
+})
+self.settings["userlist.json"].set_data("hidden-users",{
+    "label":_("Hidden users list"),
+     "value": config.get("hidden-users","root")
+})
+
