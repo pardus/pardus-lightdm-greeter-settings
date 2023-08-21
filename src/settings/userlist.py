69bb653d0f05ed758f604c6b4e75ccdd52d7a111
aliriza <aliriza.keskin@pardus.org.tr>
2023-08-21 11:20:01 +0000

    minor fixes

--- src/settings/userlist.py
+++ src/settings/userlist.py
@@ -12,7 +12,3 @@ self.settings["userlist"].set_data("user-hide-button", {
     "label": _("Enable hide user button"),
     "value": config.get("user-hide-button", "true", "userlist")
 })
-self.settings["userlist"].set_data("hidden-users", {
-    "label": _("Hidden users list"),
-    "value": config.get("hidden-users", "root", "userlist")
-})
