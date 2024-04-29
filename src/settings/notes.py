1d57a21efd2c6385bd0fc5d1a9c9ef3cb09ab65b
Ali Riza KESKIN <aliriza.keskin@pardus.org.tr>
2024-04-29 10:15:33 +0300

    fix settings reading for notes

--- src/settings/notes.py
+++ src/settings/notes.py
@@ -2,13 +2,13 @@
 _("Notes")
 self.settings["notes"].set_data("enabled", {
     "label": _("Enable notes"),
-    "value": config.get("notes", "true", "enabled"),
+    "value": config.get("enabled", "true", "notes"),
 })
 self.settings["notes"].set_data("editable", {
     "label": _("Editable"),
-    "value": config.get("editable", "true", "editable"),
+    "value": config.get("editable", "true", "notes"),
 })
 self.settings["notes"].set_data("text-file", {
     "label": _("Text file"),
-    "value": config.get("text-file", "", "editable"),
+    "value": config.get("text-file", "", "notes"),
 })
